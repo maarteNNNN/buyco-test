e96a3d9afbe152fe3ee18b858eac972def71a7af
Maarten Coppens <martinuske@gmail.com>
2020-08-12 19:41:07 -0300

    FEATURE: Pages are working FIX: Final page should be -1

--- client/src/store/modules/users.js
+++ client/src/store/modules/users.js
@@ -6,10 +6,14 @@ export const users = {
     users: [],
     page: 1,
     pages: null,
-    currentIndex: 0,
+    itemsPerPage: 10,
+    shownUsers: [],
   },
   mutations: {
-    SET_USERS: (s, d) => (s.users = d),
+    SET_USERS: (s, d) => {
+      s.users = d
+      s.shownUsers = d.slice(0, s.itemsPerPage)
+    },
     SET_PAGES: (s, d) => (s.pages = d.length / 10),
     ADD_USER: (s, d) => s.users.push(d),
     EDIT_USER: (s, d) =>
@@ -23,12 +27,20 @@ export const users = {
         s.users.findIndex(user => user.id === 1),
         1,
       ),
-    CHANGE_PAGE: (s, c) =>
-      s.page + c >= 1 && s.page + c <= s.pages && (s.page += c),
+    CHANGE_PAGE: (s, c) => {
+      s.page + c >= 1 && s.page + c <= s.pages && (s.page += c)
+      s.shownUsers = s.users.slice(
+        s.page * s.itemsPerPage,
+        s.page * s.itemsPerPage + s.itemsPerPage,
+      )
+    },
   },
   getters: {
     getUsersByPage: s =>
-      s.users.slice(s.currentIndex * s.page, s.currentIndex * s.page + 10),
+      s.users.slice(
+        s.currentIndex * s.page,
+        s.currentIndex * s.page + s.itemsPerPage,
+      ),
     getUserById: s => id => s.users.find(user => user.id === id),
     orderById: s => ord =>
       s.users.sort((a, b) => (ord === 'ASC' ? a - b : b - a)),
--- client/src/views/users/UserList.vue
+++ client/src/views/users/UserList.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   div
-    p(v-for="user in getUsersByPage" :key="user.id") {{user.id }} - {{ user.firstName }} {{ user.lastName }}
+    p(v-for="user in shownUsers" :key="user.id") {{user.id }} - {{ user.firstName }} {{ user.lastName }}
     span(v-if='page !== 1' @click='changePage(-1)') Previous
-    span(v-if='page !== pages' @click='changePage(+1)') Next
+    span(v-if='page !== pages - 1' @click='changePage(+1)') Next
 </template>
 
 <script>
@@ -11,7 +11,7 @@ import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'UsersList',
   computed: {
-    ...mapState('users', ['users', 'page', 'pages']),
+    ...mapState('users', ['users', 'page', 'pages', 'shownUsers']),
     ...mapGetters('users', ['getUsersByPage']),
   },
   created: function() {
